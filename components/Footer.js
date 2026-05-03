f481cd0774bde3ad4b36f4b329e21b8bfdbbe60d
Adam Muri Pamungkas <mk.adam.pamungkas@pertamina.com>
2026-05-03 17:28:56 +0700

    push new

--- components/Footer.js
+++ components/Footer.js
@@ -33,7 +33,7 @@ export default function Footer() {
           <h3 className="font-semibold text-white mb-3">Contact</h3>
           <ul className="space-y-2 text-sm">
             <li>kelompok4ptikb@gmail.com</li>
-            <li>Universitas Negeri Malang</li>
+            <li>Universitas Negeri Medan</li>
           </ul>
         </div>
 
