ca15bc80adf3569c92a124c618b974a4a1eb5bca
Adam Muri Pamungkas <mk.adam.pamungkas@pertamina.com>
2026-05-03 14:24:12 +0700

    add

--- app/register/page.js
+++ app/register/page.js
@@ -58,6 +58,13 @@ export default function Register() {
           className="w-full mb-3 p-3 border rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
           onChange={(e) => setForm({ ...form, username: e.target.value })}
         />
+    {/* name */}
+        <input
+          type="text"
+          placeholder="nama"
+          className="w-full mb-3 p-3 border rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
+          onChange={(e) => setForm({ ...form, username: e.target.value })}
+        />
 
         {/* EMAIL */}
         <input
